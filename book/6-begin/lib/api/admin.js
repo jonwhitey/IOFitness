08ca79bb7234e91a8313a173970b0f0b14fe2abd
Jonathan White <jonathan42white@gmail.com>
2019-07-27 10:21:43 -0500

    first commit

---
+++ book/6-begin/lib/api/admin.js
@@ -0,0 +1,38 @@
+import sendRequest from "./sendRequest";
+
+const BASE_PATH = "/api/v1/admin";
+
+export const getBookList = () =>
+  sendRequest(`${BASE_PATH}/books`, {
+    method: "GET"
+  });
+
+export const addBook = ({ name, price, githubRepo }) =>
+  sendRequest(`${BASE_PATH}/books/add`, {
+    body: JSON.stringify({ name, price, githubRepo })
+  });
+
+export const editBook = ({ id, name, price, githubRepo }) =>
+  sendRequest(`${BASE_PATH}/books/edit`, {
+    body: JSON.stringify({
+      id,
+      name,
+      price,
+      githubRepo
+    })
+  });
+
+export const getBookDetail = ({ slug }) =>
+  sendRequest(`${BASE_PATH}/books/detail/${slug}`, {
+    method: "GET"
+  });
+
+export const syncBookContent = ({ bookId }) =>
+  sendRequest(`${BASE_PATH}/books.synch-content`, {
+    body: JSON.stringify({ bookId })
+  });
+
+export const getGitHubRepos = () =>
+  sendRequest(`${BASE_PATH}/github/repos`, {
+    method: "GET"
+  });
