7668d6dd217119f4bf89d814981c2b602b1304ee
Jonathan White <jonathan42white@gmail.com>
2019-08-28 17:35:48 -0500

    merge

--- server/database.js
+++ server/database.js
@@ -1,5 +1,13 @@
 const { options, MONGO_URL } = './app.js';
 const mongoose = require('mongoose');
+<<<<<<< HEAD
+
+=======
+const { mongoose,
+        options,
+        MONGO_URL,} = require('../app.js');
+const dev = process.env.NODE_ENV !== 'production';
+>>>>>>> parent of 06ff214... Begin test suite 2
 
 
 const connect = async () => {
