9e0e42050bace4aead31198a510fc46623d9c076
Jonathan White <jonathan42white@gmail.com>
2020-04-09 10:57:12 -0500

    merge deleteUser branch

--- pages/customer/my-account.js
+++ pages/customer/my-account.js
@@ -1,21 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import TextField from "@material-ui/core/TextField";
+import React from 'react';
+import PropTypes from 'prop-types';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import NProgress from 'nprogress';
 import { deleteUser } from '../../lib/api/auth';
 
-
-import withAuth from "../../lib/withAuth";
-import {
-  styleForm,
-  styleTextField
-} from "../../components/SharedStyles";
+import withAuth from '../../lib/withAuth';
+import { styleForm, styleTextField } from '../../components/SharedStyles';
 
 // Index is a little dashboard that shows the functionality of withAuth
 // withAuth passed the user object as a prop to Index
@@ -28,42 +24,40 @@ class MyAccount extends React.Component {
       email: '',
       password: '',
       formErrors: { email: '', password: '' },
-    }
+    };
     this.handleUserInput = this.handleUserInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  };
+  }
 
   handleSubmit = async (e) => {
     NProgress.start();
-    const {email, password} = this.state;
+    const { email, password } = this.state;
     try {
-      e.preventDefault()
+      e.preventDefault();
       console.log('HANDLE SUBMIT');
       console.log(email + password);
-      await deleteUser({email, password});
+      await deleteUser({ email, password });
       window.location.reload(true);
     } catch (e) {
-      this.setState({formErrors: { email: 'It didnt work!' } })
+      this.setState({ formErrors: { email: 'It didnt work!' } });
     }
-  }; 
+  };
 
   handleUserInput = (e) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const { target } = e;
+    const { value } = target;
+    const { name } = target;
 
     this.setState({
-      [name]: value
+      [name]: value,
     });
     console.log(this.state);
   };
 
-
-
   render() {
-    const { formErrors, email, password } = this.state;
+    const { formErrors } = this.state;
     return (
-      <div style={{ padding: "10px 45px" }}>
+      <div style={{ padding: '10px 45px' }}>
         <h1 id="my-account"> My Account Page </h1>
         <ExpansionPanel>
           <ExpansionPanelSummary
@@ -74,53 +68,51 @@ class MyAccount extends React.Component {
             <Typography>Delete Account</Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-          <form style={styleForm} noValidate onSubmit={this.handleSubmit}>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    label="Email Address"
-                    name="email"
-                    //value={email}
-                    onChange={this.handleUserInput}
-                    autoFocus
-                    style={styleTextField}
-                    error={!!formErrors.email}
-                    id="outlined-error-helper-text"
-                    helperText={formErrors.email}
-                    //value={this.state.email}
-                  />
+            <form style={styleForm} noValidate onSubmit={this.handleSubmit}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Email Address"
+                name="email"
+                // value={email}
+                onChange={this.handleUserInput}
+                autoFocus
+                style={styleTextField}
+                error={!!formErrors.email}
+                id="outlined-error-helper-text"
+                helperText={formErrors.email}
+              />
 
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    //value={password}
-                    name="password"
-                    label="Password"
-                    type="password"
-                    onChange={this.handleUserInput}
-                    style={styleTextField}
-                    error={!!formErrors.password}
-                    id="outlined-error-helper-text"
-                    helperText={formErrors.password}
-                    //value={this.state.password}
-                  />
-                  <br />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                // value={password}
+                name="password"
+                label="Password"
+                type="password"
+                onChange={this.handleUserInput}
+                style={styleTextField}
+                error={!!formErrors.password}
+                id="outlined-error-helper-text"
+                helperText={formErrors.password}
+              />
+              <br />
 
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    id="delete-user-button"
-                    onClick={this.handleSubmit}
-                  >
-                    Delete Account
-                  </Button>
-              </form>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                id="delete-user-button"
+                onClick={this.handleSubmit}
+              >
+                Delete Account
+              </Button>
+            </form>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </div>
