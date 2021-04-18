a671d92d827e424d8503990de6a8fee31d0784d7
Jonathan White <jonathan42white@gmail.com>
2021-04-18 07:12:14 -0500

    testing

--- test/mocha.teardown.js
+++
@@ -1,9 +0,0 @@
-/*
-const mongoose = require('mongoose');
-
-after(async () => {
- // await User.deleteOne({ email: 'jonathan.e.white@colorado.edu' });
-  await mongoose.connection.close();
-  console.log('Mongoose connection closed');
-});
-*/
